01e2ca848789351253002a0db31913b4962c6f66
Harhat18 <harunhatib18@gmail.com>
2024-02-29 22:50:59 +0300

    Admin side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,85 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductlistItem";
+import Button from "@components/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/src/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { addItem } = useCart();
+
+  const router = useRouter();
+
+  const { id } = useLocalSearchParams();
+
+  const [selctedSize, setSelectedSize] = useState<PizzaSize>("M");
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selctedSize);
+    router.push("/cart");
+  };
+
+  if (!product) {
+    return <Text>Product Not Found</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        source={{ uri: product.image } || defaultPizzaImage}
+        style={styles.image}
+      />
+      <Text style={styles.price}>{product.name}</Text>
+      <Text style={styles.title}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
+
+export default ProductDetailsScreen;
